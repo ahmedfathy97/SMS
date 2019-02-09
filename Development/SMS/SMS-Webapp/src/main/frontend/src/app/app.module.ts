1095b17833bf4dd22d980641fec8e4360e7b3aee
Ahmed Mater <ahmedmotair@gmail.com>
2019-02-09 06:30:36 +0200

    Remove Shared

--- Development/SMS/SMS-Webapp/src/main/frontend/src/app/app.module.ts
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/app.module.ts
@@ -2,29 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FullLayoutComponent } from './shared/components/full-layout/full-layout.component';
-import { SimpleLayoutComponent } from './shared/components/simple-layout/simple-layout.component';
-import { SmsHeaderComponent } from './shared/components/full-layout/components/sms-header/sms-header.component';
-import { SmsSideBarComponent } from './shared/components/full-layout/components/sms-side-bar/sms-side-bar.component';
-import { SmsFooterComponent } from './shared/components/full-layout/components/sms-footer/sms-footer.component';
+import { FullLayoutComponent } from './layout/components/full-layout/full-layout.component';
+import { SimpleLayoutComponent } from './layout/components/simple-layout/simple-layout.component';
+import { SmsHeaderComponent } from './layout/components/full-layout/components/sms-header/sms-header.component';
+import { SmsSideBarComponent } from './layout/components/full-layout/components/sms-side-bar/sms-side-bar.component';
+import { SmsFooterComponent } from './layout/components/full-layout/components/sms-footer/sms-footer.component';
 import { HomeComponent } from './modules/home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './modules/security/components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     FullLayoutComponent,
     SimpleLayoutComponent,
     SmsHeaderComponent,
     SmsSideBarComponent,
     SmsFooterComponent,
-    HomeComponent
+    HomeComponent,
+    LayoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
