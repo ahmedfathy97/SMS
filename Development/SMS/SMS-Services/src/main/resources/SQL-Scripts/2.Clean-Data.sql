b274d08613c6fb861da12e259b19946969c3518a
YaraAbdElrahman <43352276+YaraAbdElrahman@users.noreply.github.com>
2019-02-10 23:06:57 +0200

    Clean Data

--- Development/SMS/SMS-Services/src/main/resources/SQL-Scripts/2.Clean-Data.sql
+++ Development/SMS/SMS-Services/src/main/resources/SQL-Scripts/2.Clean-Data.sql
@@ -1,8 +1,8 @@
-# INSERTion courseTable
-INSERT INTO `sms`.`course` (`id`) VALUES ('1');
-INSERT INTO `sms`.`course` (`id`) VALUES ('2');
-INSERT INTO `sms`.`course` (`id`) VALUES ('3');
-INSERT INTO `sms`.`course` (`id`) VALUES ('4');
+#INSERTion user_auth
+INSERT INTO `sms`.`auth_user` (`id`, `user_name`, `user_pass`, `first_name`, `last_name`)
+VALUES ('1', 'ahmed matar', '1234', 'ahmed', 'matar');
+INSERT INTO `sms`.`auth_user` (`id`, `user_name`, `user_pass`, `first_name`, `last_name`)
+VALUES ('2', 'ahmed taha', '1233', 'ahmed', 'taha');
 
 # INSERTion courseCategoryTable
 INSERT INTO `sms`.`cor_category` (`id`, `label_en`) VALUES ('1', 'Design');
@@ -21,3 +21,8 @@ INSERT INTO `sms`.`cor_level` (`id`, `label_en`) VALUES ('3', 'Beginner');
 INSERT INTO `sms`.`cor_level` (`id`, `label_en`) VALUES ('4', 'Advanced');
 INSERT INTO `sms`.`cor_level` (`id`, `label_en`) VALUES ('5', 'Entery');
 
+#INSERTion course
+INSERT INTO `sms`.`course` (`id`, `cor_name`, `duration`, `start_date`, `end_date`, `instructor_id`, `category_id`, `type_id`, `level_id`, `description`)
+ VALUES ('1', 'java', '3', '2018-5-12', '2018-8-12', '1', '1', '1', '1', 'programming language');
+INSERT INTO `sms`.`course` (`id`, `cor_name`, `duration`, `start_date`, `end_date`, `instructor_id`, `category_id`, `type_id`, `level_id`, `description`)
+VALUES ('2', 'c++', '3', '2018-5-12', '2018-8-12', '2', '2', '2', '2', 'programming language');
