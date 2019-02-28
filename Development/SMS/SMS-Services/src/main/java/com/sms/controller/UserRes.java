db0600e204dfbbab71732ee0281ea1077f8da192
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-02-28 03:31:36 +0200

    create user profile

--- Development/SMS/SMS-Services/src/main/java/com/sms/controller/UserRes.java
+++ Development/SMS/SMS-Services/src/main/java/com/sms/controller/UserRes.java
@@ -1,18 +1,26 @@
 package com.sms.controller;
 
+import com.sms.model.user.UserVTO;
+import com.sms.repository.UserRep;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.awt.*;
+import java.util.List;
 
 @Path("/user")
 public class UserRes {
-
+    @Autowired
+    private UserRep repository;
     @GET
     @Path("/{userID}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void findByID( int userID){
+    public List<UserVTO> userData(@PathParam("userID") int userID) {
+       List<UserVTO> data = this.repository.findByID(userID);
+      return data;
 
     }
 }
