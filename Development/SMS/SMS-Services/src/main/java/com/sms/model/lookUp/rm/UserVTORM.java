bc73553b3a30009201cdc69279514b06b5d0e465
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-02-28 03:30:35 +0200

    create user profile

--- Development/SMS/SMS-Services/src/main/java/com/sms/model/lookUp/rm/UserVTORM.java
+++ Development/SMS/SMS-Services/src/main/java/com/sms/model/lookUp/rm/UserVTORM.java
@@ -1,4 +1,25 @@
 package com.sms.model.lookUp.rm;
 
-public class UserVTORM {
+import com.sms.model.user.UserVTO;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserVTORM implements RowMapper<UserVTO> {
+    @Override
+    public UserVTO mapRow(ResultSet rs, int rowIndex) throws SQLException {
+
+        UserVTO data=new UserVTO();
+        String firstName=rs.getString("first_name");
+        String lastName=rs.getString("last_name");
+        data.setId(rs.getInt("id"));
+        data.setFullName(firstName + " " +lastName);
+        data.setAge(rs.getInt("age"));
+        data.setGender(rs.getString("gender"));
+        data.setE_mail(rs.getString("e_mail"));
+        data.setPhone(rs.getInt("phone"));
+        data.setCollege(rs.getString("college"));
+        return data;
+    }
 }
