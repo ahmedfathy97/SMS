02a55e9f5e5d5b1f67aa86e047b1ecb1a95ca997
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-02-28 03:33:56 +0200

    create user profile

--- Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/user/shared/user.service.ts
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/user/shared/user.service.ts
@@ -1,11 +1,15 @@
 import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {UserVto} from "./data/user-vto";
 
 @Injectable()
 export class UserService {
-  constructor(){
+  APP_BASE_URL : string ="http://localhost:8080/api";
+  BASE_URL : string ="/user/";
+  constructor( private http:HttpClient){}
 
-  }
   findByID(userID:number){
+ return this.http.get<UserVto>(this.APP_BASE_URL+this.BASE_URL+userID);
 
   }
 
