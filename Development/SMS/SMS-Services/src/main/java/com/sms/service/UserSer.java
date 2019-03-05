ab7af1837e5fa8187b98af3e9b57b7c343ee8f4f
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-03-05 23:18:37 +0200

    create user profile finish

--- Development/SMS/SMS-Services/src/main/java/com/sms/service/UserSer.java
+++ Development/SMS/SMS-Services/src/main/java/com/sms/service/UserSer.java
@@ -1,4 +1,21 @@
 package com.sms.service;
+import com.sms.model.user.UserVTO;
+import com.sms.repository.UserRep;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserSer {
+    private UserRep userRep;
+
+    @Autowired
+    public UserSer(UserRep userRep){
+        this.userRep=userRep;
+    }
+
+    public  UserVTO findByID( int userID) {
+
+        return userRep.findByID(userID);
+    }
+
+
+
 }
