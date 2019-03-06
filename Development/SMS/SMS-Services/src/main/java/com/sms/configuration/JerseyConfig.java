cc6bd16e66c23bca1f0314533c7d94e367b9c53a
AbanoubFayez <43210352+AbanoubFayez@users.noreply.github.com>
2019-03-06 22:40:24 +0200

    --Manage attachment

--- Development/SMS/SMS-Services/src/main/java/com/sms/configuration/JerseyConfig.java
+++ Development/SMS/SMS-Services/src/main/java/com/sms/configuration/JerseyConfig.java
@@ -1,7 +1,7 @@
 package com.sms.configuration;
 
-//import com.sms.controller.AttachmentRes;
-//import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import com.sms.controller.AttachmentRes;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,8 +13,8 @@ public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         register(CORSFilter.class);
-      //  register(AttachmentRes.class);
-      //  register(MultiPartFeature.class);
+        register(AttachmentRes.class);
+        register(MultiPartFeature.class);
         packages("com.sms.controller");
 
     }
--- Development/SMS/SMS-Services/src/main/java/com/sms/controller/AttachmentRes.java
+++ Development/SMS/SMS-Services/src/main/java/com/sms/controller/AttachmentRes.java
@@ -1,36 +1,32 @@
-//package com.sms.controller;
-//
-//import ch.qos.logback.core.util.Loader;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.sms.model.attachment.File;
-//import com.sms.service.AttachmentSer;
-//import com.sun.xml.internal.ws.api.ResourceLoader;
-//import org.glassfish.jersey.media.multipart.*;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import javax.ws.rs.*;
-//import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.nio.file.FileAlreadyExistsException;
-//import java.nio.file.FileSystems;
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
-//import java.util.List;
-//import java.util.stream.Stream;
-//
-//
-//@Path("/attachment")
-//public class AttachmentRes {
-//    private AttachmentSer service;
-//
-//    @Autowired
-//    public AttachmentRes(AttachmentSer attachmentSer) {
-//        this.service = attachmentSer;
-//    }
-//
+package com.sms.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sms.model.attachment.File;
+import com.sms.repository.AttachmentRep;
+import com.sms.service.AttachmentSer;
+import org.glassfish.jersey.media.multipart.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+
+@Path("/attachment")
+public class AttachmentRes {
+    private AttachmentSer service;
+    private AttachmentRep repository;
+
+    @Autowired
+    public AttachmentRes(AttachmentSer attachmentSer, AttachmentRep attachmentRep) {
+        this.service = attachmentSer;
+        this.repository = attachmentRep;
+
+    }
+
 //    /* Upload Multiple Files */
 //    @Path("/files")
 //    @POST
@@ -62,179 +58,98 @@
 //        }
 //        return Response.ok(fileDetails.toString()).build();
 //    }
-//
-//    /*******************************************************************************/
+
+    /*******************************************************************************/
 //    @GET
 //    @Produces(MediaType.APPLICATION_JSON)
 //    @Path("/{fileID}")
-//    public List<File> getFileById(@PathParam("fileID") int fileID) {
-//
+//    public List<File> getFilesByCategoryID(
+////            @PathParam("fileID") int categoryID
+//    ) throws IOException {
+////        List<java.io.File> files;
+////        try (Stream<java.nio.file.Path> paths = Files.walk(Paths.get("/Users/bebo/Documents/Files"))) {
+////            paths
+////                    .filter(Files::isRegularFile)
+////                    .forEach(System.out::println);
+////        }
+////      catch (IOException e){
+////            e.printStackTrace();
+////      }
+////        java.io.File f = null;
+////        String[] paths;
+////        try {
+////            // create new file
+////            f = new java.io.File("/Users/bebo/Documents/Files");
+////            // array of files and directory
+////            paths = f.list();
+////            // for each name in the path array
+////            for(String path:paths)
+////                // prints filename and directory name
+////                System.out.println(path);
+////        } catch(Exception e) {
+////            // if any error occurs
+////            e.printStackTrace();
+////        }
 //        return null;
 //    }
-//
-////    @GET
-////    @Produces(MediaType.APPLICATION_JSON)
-////    @Path("/{fileID}")
-////    public List<File> getFilesByCategoryID(
-//////            @PathParam("fileID") int categoryID
-////    ) throws IOException {
-//////        List<java.io.File> files;
-//////        try (Stream<java.nio.file.Path> paths = Files.walk(Paths.get("/Users/bebo/Documents/Files"))) {
-//////            paths
-//////                    .filter(Files::isRegularFile)
-//////                    .forEach(System.out::println);
-//////        }
-//////      catch (IOException e){
-//////            e.printStackTrace();
-//////      }
-//////        java.io.File f = null;
-//////        String[] paths;
-//////        try {
-//////            // create new file
-//////            f = new java.io.File("/Users/bebo/Documents/Files");
-//////            // array of files and directory
-//////            paths = f.list();
-//////            // for each name in the path array
-//////            for(String path:paths)
-//////                // prints filename and directory name
-//////                System.out.println(path);
-//////        } catch(Exception e) {
-//////            // if any error occurs
-//////            e.printStackTrace();
-//////        }
-////        return null;
-////    }
-//
-//    /***********************************Delete Files********************************************/
-//
-//    @DELETE
-//    @Path("/{fileID}")
-//    public void removeFile(@PathParam("fileID") int id) {
-//
-//    }
-//
-//    /*Output stream version of uploading a single file*/
-//    @Path("/file")
-//    @POST
-//    @Consumes(MediaType.MULTIPART_FORM_DATA)
-//    public Response uploadFile(
-//            @QueryParam("name") String name,
-//            @QueryParam("type") String type,
-//            @QueryParam("size") long size,
-//            @QueryParam("ext") String ext,
-//            @FormDataParam("file") InputStream fileContent,
-//            @FormDataParam("file") FormDataContentDisposition fileDisposition) {
-//
-//        String fileName = fileDisposition.getFileName();
-//        String filePath = "/Users/bebo/Documents/Files/";
-//
-//        File file = new File();
-//        file.setName(name);
-//        file.setType(type);
-//        file.setSize(size);
-//        file.setExtension(ext);
-//        file.setFile_path(filePath);
-//        file.setSourceID(1);
-//        file.setUpload_date();
-//
-//        String fileDetails = "File saved at " + filePath;
-//
-//        System.out.println(file.toString());
-//
-//        service.saveFile(fileContent, name, file);
-//        return Response.ok(fileDetails).build();
-//    }
-//
-//    /***********************************Delete Files********************************************/
-//    @Path("view")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @GET
-//    public List<File> getFilesInfo() {
-//
-//        return null;
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    /* Upload single File
-//     * Mainly used with Student uploading homework */
-//
-////    @Path("/file")
-////        @POST
-////        @Consumes(MediaType.MULTIPART_FORM_DATA)
-////        public Response uploadFile(@DefaultValue("") @FormDataParam("tags") String tags,
-////                                   @FormDataParam("file") InputStream file,
-////                                   @FormDataParam("file") FormDataContentDisposition fileDisposition,
-////                                   @FormDataParam("ID") String id) {
-////
-////
-////             String fileName = fileDisposition.getFileName();
-////
-////            saveFile(file, fileName);
-////
-////            String fileDetails = "File saved at /Users/bebo/Documents/" + fileName + " with tags "+ tags;
-////
-////            System.out.println("ID" + id);
-////
-////            System.out.println(fileDetails);
-////
-////            return Response.ok(fileDetails).build();
-////        }
-//
-//}
-//
-//
-////    @Path("/files2")
-////    @POST
-////    @Consumes(MediaType.MULTIPART_FORM_DATA)
-////    public Response uploadFiles(final FormDataMultiPart multiPart) {
-////
-////        List<FormDataBodyPart> bodyParts = multiPart.getFields("files");
-////
-////        StringBuffer fileDetails = new StringBuffer("");
-////
-////        /* Save multiple files */
-////        for (int i = 0; i < bodyParts.size(); i++) {
-////            BodyPartEntity bodyPartEntity = (BodyPartEntity) bodyParts.get(i).getEntity();
-////            String fileName = bodyParts.get(i).getContentDisposition().getFileName();
-////            saveFile(bodyPartEntity.getInputStream(), fileName);
-////            fileDetails.append(" File saved at /Volumes/Drive2/temp/file/" + fileName);
-////        }
-////
-////        /* Save File 2 */
-////
-////        BodyPartEntity bodyPartEntity = ((BodyPartEntity) multiPart.getField("file2").getEntity());
-////        String file2Name = multiPart.getField("file2").getFormDataContentDisposition().getFileName();
-////        saveFile(bodyPartEntity.getInputStream(), file2Name);
-////        fileDetails.append(" File saved at /Volumes/Drive2/temp/file/" + file2Name);
-////
-////        fileDetails.append(" Tag Details : " + multiPart.getField("tags").getValue());
-////        System.out.println(fileDetails);
-////
-////        return Response.ok(fileDetails.toString()).build();
-////    }
-//
+
+    /***********************************Delete Files********************************************/
+
+    @DELETE
+    @Path("/{fileID}")
+    public void removeFile(@PathParam("fileID") int id) {
+
+    }
+
+    /*********************************** Upload a single File ********************************************/
+    /*Output stream version of uploading a single file*/
+    @Path("/file")
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @QueryParam("name") String name,
+            @QueryParam("type") String type,
+            @QueryParam("size") long size,
+            @QueryParam("ext") String ext,
+            @FormDataParam("file") InputStream fileContent,
+            @FormDataParam("file") FormDataContentDisposition fileDisposition) {
+
+        String fileName = fileDisposition.getFileName();
+        String filePath = "/Users/bebo/Documents/Files/";
+
+        File file = new File();
+        file.setName(name);
+        file.setContentType(type);
+        file.setSize(size);
+        file.setExtension(ext);
+        file.setFile_path(filePath);
+        file.setSourceID(1);
+        file.setUpload_date();
+
+        String fileDetails = "File saved at " + filePath;
+
+        System.out.println(file.toString());
+//        getFileBySourceId(1);
+        service.saveFile(fileContent, name, file);
+
+        return Response.ok(fileDetails).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/files/{sourceID}")
+    public List<File> getFilesBySourceId(@PathParam("sourceID") int sourceID) {
+        List<File> files = this.repository.findFiles(sourceID);
+        return files;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/file/{fileID}")
+    public File getFileById(@PathParam("fileID") int fileID) {
+        service.getFile(fileID);
+        return null;
+
+    }
+
+}
