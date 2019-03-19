6b88d1c2b165c7dc1c59fdbf9662037eee605e17
ahmedfathy97 <43169681+ahmedfathy97@users.noreply.github.com>
2019-03-19 21:38:52 +0200

    View Course Quizes

---
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/course/components/quiz/view-course-quizes/view-course-quizes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-course-quizes',
+  templateUrl: './view-course-quizes.component.html',
+  styleUrls: ['./view-course-quizes.component.scss']
+})
+export class ViewCourseQuizesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
