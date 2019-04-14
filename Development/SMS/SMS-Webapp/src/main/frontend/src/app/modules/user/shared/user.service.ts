10ca8be48d6752d780e47b2f0dffda9a3de31848
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-04-14 02:40:41 +0200

    create user edit

--- Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/user/shared/user.service.ts
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/user/shared/user.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {UserVto} from "./data/user-vto";
 import {Observable} from "rxjs/internal/Observable";
+import {EditVto} from "./data/edit-vto";
 @Injectable()
 export class UserService {
   // APP_BASE_URL : string ="http://localhost:8080/api";
@@ -11,6 +12,11 @@ export class UserService {
   findByID(userID:number){
  //return this.http.get<UserVto>(this.APP_BASE_URL+this.BASE_URL+userID);
     return this.http.get<UserVto>("http://localhost:8080/api/user/"+userID);
+  }
+
+  findBYID(userID:number){
+    return this.http.get<EditVto>("http://localhost:8080/api/edit/"+userID);
+
   }
 
   // public uploadImage(image: File): Observable<Response> {
