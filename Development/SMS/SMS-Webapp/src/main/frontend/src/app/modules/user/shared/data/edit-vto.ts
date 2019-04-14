b9ee89d3ce6f8fa889bed18cff4821b52873226d
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-04-14 02:39:15 +0200

    create user edit

---
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/user/shared/data/edit-vto.ts
@@ -0,0 +1,11 @@
+export class EditVto {
+  fullName:string;
+  age:number;
+  gender:string;
+  e_mail:string;
+  phone:number;
+  college:string;
+  userName:string;
+  // profileImageURL:string;
+
+}
