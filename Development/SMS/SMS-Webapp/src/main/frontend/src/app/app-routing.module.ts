1009e2b69a9a11b44279ca7aeafc7d018fccb91b
ahmedfathy97 <43169681+ahmedfathy97@users.noreply.github.com>
2019-04-17 20:12:02 +0200

    edit structure

--- Development/SMS/SMS-Webapp/src/main/frontend/src/app/app-routing.module.ts
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/app-routing.module.ts
@@ -4,104 +4,66 @@ import {FullLayoutComponent} from "./layout/components/full-layout/full-layout.c
 import {HomeComponent} from "./modules/home/home.component";
 import {SimpleLayoutComponent} from "./layout/components/simple-layout/simple-layout.component";
 import {LoginComponent} from "./modules/security/components/login/login.component";
-import {CreateGradeComponent} from "./modules/course/components/grade/create-grade/create-grade.component";
-import {CreateAttendanceComponent} from "./modules/course/components/attendance/create-attendance/create-attendance.component";
+import {CreateGradeComponent} from "./modules/course/components/course-details/course-content/grade/create-grade/create-grade.component";
+import {CreateAttendanceComponent} from "./modules/course/components/course-details/course-content/attendance/create-attendance/create-attendance.component";
 import {CreateCourse} from "./modules/course/components/create-course/create-course.component";
-import {ViewAttendanceComponent} from "./modules/course/components/attendance/view-attendance/view-attendance.component";
-import {CreateQuizComponent} from "./modules/course/components/quiz/create-quiz/create-quiz.component";
+import {ViewAttendanceComponent} from "./modules/course/components/course-details/course-content/attendance/view-attendance/view-attendance.component";
+import {CreateQuizComponent} from "./modules/course/components/course-details/course-content/quiz/create-quiz/create-quiz.component";
 import {UserProfileComponent} from "./modules/user/components/user-profile/user-profile.component";
-import {ViewGradeComponent} from "./modules/course/components/grade/view-grade/view-grade.component";
-import {ViewStudentGradeComponent} from "./modules/course/components/grade/view-student-grade/view-student-grade.component";
-import {ViewStudentAttendanceComponent} from "./modules/course/components/attendance/view-student-attendance/view-student-attendance.component";
-import {AddQuizQuestionsComponent} from "./modules/course/components/quiz/add-quiz-questions/add-quiz-questions.component";
-import {CreateLecture} from "./modules/course/components/create-lecture/create-lecture.component";
+import {ViewGradeComponent} from "./modules/course/components/course-details/course-content/grade/view-grade/view-grade.component";
+import {ViewStudentGradeComponent} from "./modules/course/components/course-details/course-content/grade/view-student-grade/view-student-grade.component";
+import {ViewStudentAttendanceComponent} from "./modules/course/components/course-details/course-content/attendance/view-student-attendance/view-student-attendance.component";
+import {AddQuizQuestionsComponent} from "./modules/course/components/course-details/course-content/quiz/add-quiz-questions/add-quiz-questions.component";
+import {CreateLecture} from "./modules/course/components/course-details/course-content/lecture/create-lecture/create-lecture.component";
 
 import {UploadComponent} from "./infrastructure/components/manage-attachment/components/upload/upload.component";
 import {DownloadComponent} from "./infrastructure/components/manage-attachment/components/download/download.component";
-import {AnswerQuestionsComponent} from "./modules/course/components/quiz/answer-questions/answer-questions.component";
-import {LectureDetailsComponent} from "./modules/course/components/lecture-details/lecture-details.component";
+import {AnswerQuestionsComponent} from "./modules/course/components/course-details/course-content/quiz/answer-questions/answer-questions.component";
 import {UserEditComponent} from "./modules/user/components/user-edit/user-edit.component";
 import {RegisterComponent} from "./modules/security/components/register/register.component";
-import {CourseQuizesComponent} from "./modules/course/components/course-details/course-quizes/course-quizes.component";
+import {CourseQuizesComponent} from "./modules/course/components/course-details/course-content/quiz/course-quizes/course-quizes.component";
+import {CourseLecturesComponent} from "./modules/course/components/course-details/course-content/lecture/course-lectures/course-lectures.component";
+import {CourseDetailsComponent} from "./modules/course/components/course-details/course-details.component";
+import {CourseInfoComponent} from "./modules/course/components/course-details/course-content/course-info/course-info.component";
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: '', component: SimpleLayoutComponent, children: [
       {path: 'login', component: LoginComponent},
       {path: 'register', component: RegisterComponent},
-      {path: 'home', component: HomeComponent}
-    ]
-  },
-
-  {path: '', component: FullLayoutComponent, children: [
-      // {path: 'lecture', component: CreateLecture},
-      // OLD ROUTING HALA
-      {path: 'create-grade', component: CreateGradeComponent},
-      {path: 'view-grade', component: ViewGradeComponent},
-      {path: 'std-grade', component: ViewStudentGradeComponent},
-
-      // // OLD ROUTING ABANOUB
-      // {path: 'attachment/file/upload', component: UploadComponent},
-      // {path: 'attachment/file/download', component: DownloadComponent},
-      //
-      // //OLD ROUNTING YOUSEF
-      // {path: 'course/new', component: CreateCourse},
-      // {path: 'course/:corID', component: ViewCourseComponent},
-      //
-      // //OLD Routing Yara
-      {path: 'attend', component: CreateAttendanceComponent},
-      {path: 'view-attend', component: ViewAttendanceComponent},
-      {path: 'view-student-attend', component: ViewStudentAttendanceComponent},
-      // //{path: 'tutorials', component: TutorialsComponent},
-      //
-      // //OLD ROUTING Ahmed
-      // {path: 'course/quiz/new', component: CreateQuizComponent},
-      // {path: 'course/quiz/add_questions', component: AddQuizQuestionsComponent},
-      // {path: 'course/quiz/answer_questions', component: AnswerQuestionsComponent},
-      // {path: 'course/quizes', component: CourseQuizesComponent},
-      // {path: 'course/:corID/course_details', component: CourseDetailsComponent},
-      //
-      //Manar
-      {path: 'profile', children: [{path: ':userID', component: UserProfileComponent}]},
-
-      {path:'edit',children:[{path:':userID',component:UserEditComponent}]},
+      {path: 'home', component: HomeComponent} ,
 
       /////////////////////////////// Start  New Routing /////////////////////////////////////
       {
         path: 'course', /* component which view all courses*/ children: [
           {path: 'new', component: CreateCourse},
           {
-            path: ':courseID', children: [
-             // {path: 'dashboard', component: CourseDetailsComponent},
-              {
-                path: 'lecture', children: [
-                  {path: 'new', component: CreateLecture},
-                  {
-                    path: ':lectureID', component: LectureDetailsComponent, childern: [
-                      // {path:'dashboard' ,} ,
-                      {path: 'upload ', component: UploadComponent},
-                      {path: 'download', component: DownloadComponent},
-
-
-                      // {path:'article'} ,
-                      //
-                      // {path: 'attendance', children:[
-                      //     {path: 'new'},
-                      //     {path: ':stdID',},
-                      //   ]
-                      // },
-                      //
-                      // {path: 'assigment', children:[
-                      //     {path: 'new' },
-                      //     {path: ':studentID'},
-                      //   ]
-                      // },
-
-
-                    ]
-                  },
-                ]
-              },
+            path: ':courseID'  , children: [
+              {path: 'details', component: CourseDetailsComponent},
+              // {
+              //   path: 'lecture',component :CourseInfoComponent , children: [
+              //     {path: 'new', component: CreateLecture},
+              //     {
+              //       path: ':lectureID', childern: [
+              //         // {path:'dashboard' ,} ,
+              //         {path: 'upload ', component: UploadComponent},
+              //         {path: 'download', component: DownloadComponent},
+              //
+              //
+              //         // {path:'article'} ,
+              //         //
+              //         //
+              //         // {path: 'assigment', children:[
+              //         //     {path: 'new' },
+              //         //     {path: ':studentID'},
+              //         //   ]
+              //         // },
+              //
+              //
+              //       ]
+              //     },
+              //   ]
+              // },
 
               {
                 path: 'attendance', children: [
@@ -130,7 +92,6 @@ const routes: Routes = [
                     path: ':quizID', children: [
                       {path: ':studentID/answerQuiz', component: AnswerQuestionsComponent},
 
-
                     ]
                   },
                 ]
@@ -142,6 +103,42 @@ const routes: Routes = [
         ]
       },
       /////////////////////////////// END New Routing /////////////////////////////////////
+    ]
+  },
+
+  {path: '', component: FullLayoutComponent, children: [
+      // {path: 'lecture', component: CreateLecture},
+      // OLD ROUTING HALA
+      // {path: 'create-grade', component: CreateGradeComponent},
+      // {path: 'view-grade', component: ViewGradeComponent},
+      // {path: 'std-grade', component: ViewStudentGradeComponent},
+
+      // // OLD ROUTING ABANOUB
+      // {path: 'attachment/file/upload', component: UploadComponent},
+      // {path: 'attachment/file/download', component: DownloadComponent},
+      //
+      // //OLD ROUNTING YOUSEF
+      // {path: 'course/new', component: CreateCourse},
+      // {path: 'course/:corID', component: CourseInfoComponent},
+      //
+      // //OLD Routing Yara
+      {path: 'attend', component: CreateAttendanceComponent},
+      {path: 'view-attend', component: ViewAttendanceComponent},
+      {path: 'view-student-attend', component: ViewStudentAttendanceComponent},
+      // //{path: 'tutorials', component: TutorialsComponent},
+      //
+      // //OLD ROUTING Ahmed
+      // {path: 'course/quiz/new', component: CreateQuizComponent},
+      // {path: 'course/quiz/add_questions', component: AddQuizQuestionsComponent},
+      // {path: 'course/quiz/answer_questions', component: AnswerQuestionsComponent},
+      // {path: 'course/quizes', component: CourseQuizesComponent},
+      // {path: 'course/:corID/course_details', component: CourseDetailsComponent},
+      //
+      //Manar
+      {path: 'profile', children: [{path: ':userID', component: UserProfileComponent}]},
+
+      {path:'edit',children:[{path:':userID',component:UserEditComponent}]},
+
 
     ]
   }
--- Development/SMS/SMS-Webapp/src/main/frontend/src/app/app.module.ts
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/app.module.ts
@@ -4,32 +4,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { FullLayoutComponent } from './layout/components/full-layout/full-layout.component';
 import { SimpleLayoutComponent } from './layout/components/simple-layout/simple-layout.component';
 import { SmsHeaderComponent } from './layout/components/full-layout/components/sms-header/sms-header.component';
-import { SmsSideBarComponent } from './layout/components/full-layout/components/sms-side-bar/sms-side-bar.component';
+import { CourseSideBarComponent } from './modules/course/components/course-details/course-side-bar/course-side-bar.component';
 import { SmsFooterComponent } from './layout/components/full-layout/components/sms-footer/sms-footer.component';
 import { HomeComponent } from './modules/home/home.component';
 import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './modules/security/components/login/login.component';
-import { CreateGradeComponent } from './modules/course/components/grade/create-grade/create-grade.component';
+import { CreateGradeComponent } from './modules/course/components/course-details/course-content/grade/create-grade/create-grade.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
-import { CreateAttendanceComponent } from './modules/course/components/attendance/create-attendance/create-attendance.component';
+import { CreateAttendanceComponent } from './modules/course/components/course-details/course-content/attendance/create-attendance/create-attendance.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import { ViewGradeComponent } from './modules/course/components/grade/view-grade/view-grade.component';
-import { ViewAttendanceComponent } from './modules/course/components/attendance/view-attendance/view-attendance.component';
-import { CreateQuizComponent } from './modules/course/components/quiz/create-quiz/create-quiz.component';
+import { ViewGradeComponent } from './modules/course/components/course-details/course-content/grade/view-grade/view-grade.component';
+import { ViewAttendanceComponent } from './modules/course/components/course-details/course-content/attendance/view-attendance/view-attendance.component';
+import { CreateQuizComponent } from './modules/course/components/course-details/course-content/quiz/create-quiz/create-quiz.component';
 import { UploadComponent } from './infrastructure/components/manage-attachment/components/upload/upload.component';
 import { DownloadComponent } from './infrastructure/components/manage-attachment/components/download/download.component';
-import {AddQuizQuestionsComponent} from "./modules/course/components/quiz/add-quiz-questions/add-quiz-questions.component";
-import { ViewStudentGradeComponent } from './modules/course/components/grade/view-student-grade/view-student-grade.component';
-import { ViewStudentAttendanceComponent } from './modules/course/components/attendance/view-student-attendance/view-student-attendance.component';
+import {AddQuizQuestionsComponent} from "./modules/course/components/course-details/course-content/quiz/add-quiz-questions/add-quiz-questions.component";
+import { ViewStudentGradeComponent } from './modules/course/components/course-details/course-content/grade/view-student-grade/view-student-grade.component';
+import { ViewStudentAttendanceComponent } from './modules/course/components/course-details/course-content/attendance/view-student-attendance/view-student-attendance.component';
 import {CreateCourse} from "./modules/course/components/create-course/create-course.component";
-import {ViewCourseComponent} from "./modules/course/components/view-course/view-course.component";
-import { CreateLecture } from './modules/course/components/create-lecture/create-lecture.component';
+import {CourseInfoComponent} from "./modules/course/components/course-details/course-content/course-info/course-info.component";
+import { CreateLecture } from './modules/course/components/course-details/course-content/lecture/create-lecture/create-lecture.component';
 import {UserProfileComponent} from "./modules/user/components/user-profile/user-profile.component";
-import { AnswerQuestionsComponent } from './modules/course/components/quiz/answer-questions/answer-questions.component';
-import { CourseQuizesComponent } from './modules/course/components/course-details/course-quizes/course-quizes.component';
-import { CourseLecturesComponent } from './modules/course/components/course-details/course-lectures/course-lectures.component';
-import { LectureDetailsComponent } from './modules/course/components/lecture-details/lecture-details.component';
+import { AnswerQuestionsComponent } from './modules/course/components/course-details/course-content/quiz/answer-questions/answer-questions.component';
+
 import { UserEditComponent } from './modules/user/components/user-edit/user-edit.component';
 import {RegisterComponent} from "./modules/security/components/register/register.component";
 import {AuthorizeActionDirective} from "./infrastructure/directives/authorization/authorize-action.directive";
@@ -39,17 +37,20 @@ import {AuthorizationGuard} from "./infrastructure/interceptor/authorization.gua
 import {AuthInterceptor} from "./infrastructure/interceptor/auth-interceptor";
 import {AuthenticationGuard} from "./infrastructure/interceptor/authentication.guard";
 import {LocalStorageService} from "./infrastructure/services/local-storage.service";
+import { CourseDetailsComponent } from './modules/course/components/course-details/course-details.component';
+import {CourseQuizesComponent} from "./modules/course/components/course-details/course-content/quiz/course-quizes/course-quizes.component";
+import {CourseLecturesComponent} from "./modules/course/components/course-details/course-content/lecture/course-lectures/course-lectures.component";
 
 @NgModule({
   declarations: [
-    FullLayoutComponent, SimpleLayoutComponent, SmsHeaderComponent, SmsSideBarComponent, SmsFooterComponent,
+    FullLayoutComponent, SimpleLayoutComponent, SmsHeaderComponent, CourseSideBarComponent, SmsFooterComponent,
     HomeComponent, LayoutComponent, CreateGradeComponent, CreateAttendanceComponent, CreateCourse,
-    ViewCourseComponent, ViewAttendanceComponent, CreateQuizComponent, ViewGradeComponent,
+    CourseInfoComponent, ViewAttendanceComponent, CreateQuizComponent, ViewGradeComponent,
     UserProfileComponent, UploadComponent, DownloadComponent, ViewStudentGradeComponent ,
     AddQuizQuestionsComponent, ViewStudentAttendanceComponent, CreateLecture, AnswerQuestionsComponent,
-    CourseQuizesComponent, CourseLecturesComponent, LectureDetailsComponent,
+    CourseQuizesComponent, CourseLecturesComponent,CourseDetailsComponent ,
     UserEditComponent, RegisterComponent, LoginComponent, AuthorizeActionDirective, AuthorizeViewDirective,
-    UnAuthorizedComponent
+    UnAuthorizedComponent,
   ],
   imports: [
     BrowserModule,
