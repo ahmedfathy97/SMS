1a6d5ac72cf192d6406ed3254630b23ad47a35bb
ahmedfathy97 <43169681+ahmedfathy97@users.noreply.github.com>
2019-04-18 15:44:09 +0200

    Announcment

--- Development/SMS/SMS-Services/src/main/java/com/sms/model/course/Announcement.java
+++ Development/SMS/SMS-Services/src/main/java/com/sms/model/course/Announcement.java
@@ -7,5 +7,27 @@ public class Announcement {
     String content ;
     Date announcmentDate ;
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getAnnouncmentDate() {
+        return announcmentDate;
+    }
+
+    public void setAnnouncmentDate(Date announcmentDate) {
+        this.announcmentDate = announcmentDate;
+    }
 }
