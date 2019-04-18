9c089fd24f5c90a3f63b60954d72fb7312359dbb
YaraAbdElrahman <43352276+YaraAbdElrahman@users.noreply.github.com>
2019-04-18 20:18:09 +0200

    announc

--- Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/course/components/course-details/course-content/announcment/create-announcment/create-announcment.component.ts
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/course/components/course-details/course-content/announcment/create-announcment/create-announcment.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {AnnouncementService} from "../../../../../shared/services/announcement.service";
-import {AttendanceService} from "../../../../../shared/services/attendance.service";
 import {ActivatedRoute} from "@angular/router";
-import {AttendanceDTO} from "../../../../../shared/data/attendance-dto.data";
-import {CourseService} from "../../../../../shared/services/course.service";
-import {CreateAttendanceComponent} from "../../attendance/create-attendance/create-attendance.component";
 import {Announcement} from "../../../../../shared/data/announcement";
-import {LectureDto} from "../../../../../shared/data/lecture-dto.data";
-
 @Component({
   selector: 'app-create-announcment',
   templateUrl: './create-announcment.component.html',
@@ -22,7 +16,7 @@ export class CreateAnnouncmentComponent implements OnInit {
   corID :number ;
   formData = this.formBuilder.group({
     title: ['', Validators.required],
-    content :['', Validators.required],
+    description :['', Validators.required],
   });
 
   announcement: Announcement = new Announcement();
@@ -40,7 +34,7 @@ export class CreateAnnouncmentComponent implements OnInit {
   onSubmitNewAnnouncement() {
     let announcement: Announcement = new Announcement();
     announcement.title = this.formData.get('title').value;
-    announcement.content = this.formData.get('content').value;
+    announcement.content= this.formData.get('description').value;
     var courseID = +this.courseID;
     this.corID = courseID ;
     this.announcementService.createNewAnnouncement(this.corID ,announcement).subscribe(res => {
