86d96b205fef6a942c6305725db7d614523749ee
YaraAbdElrahman <43352276+YaraAbdElrahman@users.noreply.github.com>
2019-04-18 18:29:07 +0200

    announc

---
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/course/components/course-details/course-content/announcment/create-announcment/create-announcment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-announcment',
+  templateUrl: './create-announcment.component.html',
+  styleUrls: ['./create-announcment.component.css']
+})
+export class CreateAnnouncmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
