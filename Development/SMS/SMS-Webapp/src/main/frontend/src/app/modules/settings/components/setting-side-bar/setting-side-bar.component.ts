3b9f2acaf0edcaf9627657962eb54c08a3f5e68d
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-04-26 17:30:39 +0200

    setting side-bar

---
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/settings/components/setting-side-bar/setting-side-bar.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {AngularFullRoutes} from "../../../../infrastructure/data/full-routes.enum";
+
+@Component({
+  selector: 'app-setting-side-bar',
+  templateUrl: './setting-side-bar.component.html',
+  styleUrls: ['./setting-side-bar.component.css']
+})
+export class SettingSideBarComponent implements OnInit {
+  ROUTES: typeof AngularFullRoutes = AngularFullRoutes;
+ // replaceUserID= replaceUserID;
+  @Input() userID: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
