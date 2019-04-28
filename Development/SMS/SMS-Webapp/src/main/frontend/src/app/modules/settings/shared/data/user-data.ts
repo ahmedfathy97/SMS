94c4ad5a35b485eab53a12d9d1ac4c5a007d730f
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-04-28 21:05:49 +0200

    Setting-content

---
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/settings/shared/data/user-data.ts
@@ -0,0 +1,13 @@
+export class UserData {
+  userID:number;
+  userName: string;
+  firstName :string ;
+  lastName:string ;
+  fullName: string;
+  age: number;
+  gender: string;
+  e_mail: string;
+  phone: number;
+  college: string;
+
+}
