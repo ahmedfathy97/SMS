339dc614dff32b499920a84079bf305e7e65e293
Hala mahmoud <43175109+halamahmoud8@users.noreply.github.com>
2019-04-28 16:42:19 +0200

    alter grade

--- Development/SMS/SMS-Services/src/main/resources/SQL-Scripts/Alter.sql
+++ Development/SMS/SMS-Services/src/main/resources/SQL-Scripts/Alter.sql
@@ -3,3 +3,8 @@ INSERT INTO auth_view (id, label_ar, description_ar) VALUE (3, 'Add-Attendance',
 INSERT INTO auth_role_view (role_id, view_id) VALUE (1,3);
 INSERT INTO auth_user_view (user_id, view_id) VALUE (4,3);
 -----YARA----
+--start hala--
+INSERT INTO auth_view (id, label_ar, description_ar) VALUE (2, 'Add-Grade', '');
+INSERT INTO auth_role_view (role_id, view_id) VALUE (1,2);
+INSERT INTO auth_user_view (user_id, view_id) VALUE (4,2);
+--end hala----
