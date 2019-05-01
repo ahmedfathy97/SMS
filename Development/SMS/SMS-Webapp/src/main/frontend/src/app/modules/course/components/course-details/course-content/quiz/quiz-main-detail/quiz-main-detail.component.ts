b35fcd1a00456d8a6f42ef873c70e89276f82758
ahmedfathy97 <43169681+ahmedfathy97@users.noreply.github.com>
2019-05-01 15:03:26 +0200

    quiz detail

---
+++ Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/course/components/course-details/course-content/quiz/quiz-main-detail/quiz-main-detail.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {QuizService} from "../../../../../shared/services/quiz.service";
+import {QuizInformationVto} from "../../../../../shared/data/quiz/quiz-information-vto";
+import {ActivatedRoute} from "@angular/router";
+import {AngularFullRoutes} from "../../../../../../../infrastructure/data/full-routes.enum";
+import {CourseDataService} from "../../../../../shared/services/course-data.service";
+
+@Component({
+  selector: 'app-quiz-main-detail',
+  templateUrl: './quiz-main-detail.component.html',
+  styleUrls: ['./quiz-main-detail.component.scss'] ,
+  providers: [QuizService],
+})
+export class QuizMainDetailComponent implements OnInit {
+
+  ROUTES: typeof AngularFullRoutes = AngularFullRoutes;
+  replaceQuizID = replaceQuizID;
+  replaceCorID = replaceCorID;
+  corID: number;
+  quizId: number;
+  quizDetials :QuizInformationVto ;
+
+  constructor(private corDataService: CourseDataService,
+              private quizService: QuizService, private route: ActivatedRoute) {
+    this.route.paramMap.subscribe(params => {
+      this.quizId = +params.get("quizID");
+    }) ,
+
+      this.corDataService.corID.subscribe(
+        data => {
+          this.corID = data;
+          console.log(data);
+        }
+      );
+
+    this.corDataService.requestCorID.next(true);
+
+
+  }
+
+
+  ngOnInit() {
+    this.getQuizInformation() ;
+  }
+
+
+  getQuizInformation() {
+
+    this.quizService.getQuizDetails(this.quizId).subscribe(res => {
+      this.quizDetials = res ;
+    }, err => {
+      console.log(err);
+    });
+  }
+
+}
