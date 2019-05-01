d9091c3a1d3ac54867ab59ea78fa83c3b47b17c1
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-05-01 15:57:02 +0200

    refactor user edit

--- Development/SMS/SMS-Webapp/src/main/frontend/src/app/modules/settings/shared/data/edit-data.ts
+++
@@ -1,9 +0,0 @@
-import {Subject} from "rxjs/internal/Subject";
-import {Injectable} from "@angular/core";
-
-@Injectable()
-export class EditData {
-   userID=new Subject<number>();
-   constructor() { }
-
-}
