be902b21fd72291e6869037b8879f8b8af117923
Abanoub Fayez <43210352+AbanoubFayez@users.noreply.github.com>
2019-07-01 15:47:59 +0200

    -- Lecture-Details & Manage Attachment

--- Development/SMS/SMS-Services/src/main/resources/SQL-Scripts/Alter.sql
+++ Development/SMS/SMS-Services/src/main/resources/SQL-Scripts/Alter.sql
@@ -1,9 +1,8 @@
 ALTER TABLE `sms`.`attachment`
-    CHANGE COLUMN `content_type` `content_type` VARCHAR(100) NOT NULL,
-    CHANGE COLUMN `file_name` `file_name` VARCHAR(100) NOT NULL,
-    ADD COLUMN `start_date` DATE NULL AFTER `file_src_id`,
-    ADD COLUMN `end_date` DATE NULL AFTER `start_date`,
-    ADD COLUMN `corID` INT NULL AFTER `corID`;
+    ADD COLUMN `corID` INT NOT NULL AFTER `file_src_id`,
+    ADD COLUMN `start_date` DATE NULL DEFAULT NULL AFTER `corID`,
+    ADD COLUMN `end_date` DATE NULL DEFAULT NULL AFTER `start_date`,
+    CHANGE COLUMN `content_type` `content_type` VARCHAR(100) NOT NULL;
 
 
 
